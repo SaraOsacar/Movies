848212c41941b32c3df5d711e915fc3664758708
SaraOsacar <saraosacar@gmail.com>
2023-11-13 16:17:40 +0100

    IMG API default cover

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,35 +1,42 @@
 import React from 'react';
 
-const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const IMG_API = 'https://image.tmdb.org/t/p/w1280';
 
 const setVoteClass = (vote) => {
-    if(vote >= 8) {
-        return 'green'
+  if (vote >= 8) {
+    return 'green';
+  } else if (vote >= 6) {
+    return 'blue';
+  } else {
+    return 'red';
+  }
+};
 
-    } else if (vote >=6) {
-        return 'blue'
-    } else {
-        return 'red';
-    }
-}
-
-const Movie = ({ title, poster_path, overview, vote_average, original_language }) => (
-    <div className="movie">
-        
-<img src={IMG_API + poster_path} alt={title} />
-<div className="movie-info">
-    <h3>{title}</h3>
-    <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
-   
-</div>
+const Movie = ({
+  title,
+  poster_path,
+  overview,
+  vote_average,
+  original_language,
+}) => (
+  <div className="movie">
+    <img
+      src={poster_path ? (IMG_API + poster_path)
+          : 'https://images.unsplash.com/photo-1542204165-65bf26472b9b?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW92aWVzfGVufDB8fDB8fHww'
+      }
+      alt={title}
+    />
+    <div className="movie-info">
+      <h3>{title}</h3>
+      <span className={`tag ${setVoteClass(vote_average)}`}>
+        {vote_average}
+      </span>
+    </div>
     <div className="movie-over">
-        <h2>Overview</h2>
-        <p>{overview}</p>
-        <span>{original_language}</span>
-
+      <h2>Overview</h2>
+      <p>{overview}</p>
+      <span>{original_language}</span>
     </div>
-
-    </div>
-
+  </div>
 );
 export default Movie;
