7a0819f3d3edef5c8e9b203c3502851ee125c0c0
SaraOsacar <saraosacar@gmail.com>
2023-11-13 11:54:47 +0100

    Fix search API

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,11 @@ import Movie from './Movie';
 const FEATURED_API = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
  const IMG_API = "https://image.tmdb.org/t/p/w1280";
- const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082b1&query=";
+
+
+ const SEARCH_API =
+   'https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&query=';
+
 
 
 
@@ -27,6 +31,17 @@ function App() {
 
  const handleOnSubmit = (e) => {
   e.preventDefault();
+
+  if(searchTerm) {
+    fetch(SEARCH_API + searchTerm)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+
+    setSearchTerm('');
+  }
  };
 
  const handleOnChange = (e) => {
