da8beb381533cb48952612206ebd144d72db654f
SaraOsacar <saraosacar@gmail.com>
2023-11-28 16:37:22 +0100

    fix API

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import Movie from './Movie';
 
 const FEATURED_API =
-  'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 
 const IMG_API = 'https://image.tmdb.org/t/p/w1280';
 
